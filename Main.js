693f0b422d431ecc314dbecba12f154503b9ea63
Weirdo1_1 <samyjabarti@hotmail.com>
2023-11-20 21:02:50 +0300

    Done but forgot to push it

--- Main.js
+++ Main.js
@@ -1,1 +1,112 @@
-console.log("Welcome to JavaScript Course");
+/*let_color= 'blue';
+console.log(color);
+console.log(color);
+console.log(color);
+console.log(color);
+console.log(color);
+color = 'black';
+console.log (color) ; */
+/*
+var name = 'Ali';
+console.log (name) */
+
+/*let age = 25;
+const yearOfBirth = 1995;
+console.log(yearOfBirth);
+age = 26;
+console.log(age); */
+
+/*let name = 'Nasser';
+let age = 30;
+console.log(typeof name);
+console.log(typeof age); */
+
+/*let result = 2 < 3;
+console.log(result);
+console.log(typeof result); */
+
+/*let message = 'Welcome to JavaScript';
+console.log(typeof message);
+console.log (message); */
+
+/*let language = 'C++';
+let message = `Welcome
+to
+${language}`;
+console.log (message);*/
+
+/*let name;
+console.log(name);*/
+
+/*let value = -2.5;
+console.log(value);*/
+
+/*let value = 2;
+console.log(value * 'JavaScript');*/
+
+
+/*let value = null;
+console.log(value);*/
+
+/*let language = 'JavaScript';
+console.log(language.length);*/
+
+/*let language = ' JavaScript';
+let message = 'Welcome to' + language;
+console.log (message);*/
+
+/*let language = 'JavaScript';
+console.log(language [4]);*/
+
+/*let color = 'red';
+let colors = ['red', 'green', 'blue'];
+console.log(colors [2]);*/
+
+/*let colors = ['red', 'green', 'blue'];
+colors [1] = 'black';
+console.log(colors);*/
+
+/*let colors = ['red', 'green', 'blue', 'black'];
+console.log (colors. length);*/
+
+/*let colors = ['red', 'green', 'blue'];
+console.log(colors.includes ('black'));*/
+
+/*const colors = ['red', 'green', 'blue'];
+colors.unshift('black');
+colors.unshift( 'white');
+console.log (colors);*/
+
+/*const colors = ['red', 'green', 'blue'];
+const redColor = colors.shift();
+console.log(colors);
+console.log (redColor);*/
+
+/*const colors = ['red', 'green', 'blue'];
+colors.push('black');
+console.log(colors);*/
+
+/*const colors = ['red', 'green', 'blue'];
+let blueColor = colors.pop();
+console.log(blueColor);
+console.log (colors);*/
+
+/*let color = 'black';
+let colors = ['red', 'green', 'blue'];
+console.log(Array.isArray (colors));*/
+
+/*const values = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+    ];
+    console.log(values [2] [2]);*/
+
+/*let message = 'Welcome to JavaScript';
+let result = message.split(' ');
+console.log(result);*/
+
+/*let message = ['Welcome', 'to', 'JavaScript'];
+let result = message.join('@');
+console.log(result);*/
+
