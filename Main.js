c6538710a897a6318088c8db6bd1cf7294ea34c9
Weirdo1_1 <samyjabarti@hotmail.com>
2023-11-20 21:02:53 +0300

    final commit

--- Main.js
+++ Main.js
@@ -110,3 +110,315 @@ console.log(result);*/
 let result = message.join('@');
 console.log(result);*/
 
+/*let age = 11;
+if(age < 12) {
+console.log('Sorry, you are not old enough to play this game.');
+}*/
+
+/*let age = 15;
+if(age < 12) {
+console.log('Sorry, you are not old enough to play this game.');
+}else{
+console.log('Welcome to the game');
+}*/
+
+/*let number = 0;
+if (number > 0) {
+console.log('+');
+}else if (number < 0) {
+console.log('-');
+}else{
+console.log('zero');
+}*/
+
+/*const colors = ['red', 'green', 'blue'];
+let colorNumber = 2;
+switch(colorNumber) {
+case 1:
+console.log(colors[0]);
+break;
+case 2:
+console.log(colors [1]);
+break;
+case 3:
+console.log(colors [2]);
+break;
+}*/
+
+/*const colors = ['red', 'green', 'blue'];
+let colorNumber = 25;
+switch(colorNumber) {
+case 1:
+console.log(colors [0]);
+break;
+case 2:
+console.log (colors [1]);
+break;
+case 3:
+console.log(colors [2]);
+break;
+default:
+console.log('white');
+break;
+}*/
+
+/*const colors = ['red', 'green', 'blue'];
+let colorNumber = 6;
+switch(colorNumber) {
+case 1:
+console.log(colors [0]);
+break;
+case 2:
+console.log(colors [1]);
+break;
+case 3:
+console.log(colors [2]);
+break;
+case 4:
+case 5:
+console.log('black');
+break;
+default:
+console.log('white');
+break;
+}*/
+
+/*console.log (1);
+console.log (2);
+console.log (3);
+console.log (4);
+console.log (5);
+console.log (6);
+console.log (7);*/
+
+/*for(let i = 1; i <= 50; i++){
+    console.log(i);
+    }*/
+
+/*for(let i = 5; i >= 1; i--){
+        console.log(i);
+        }*/
+
+/*const colors = ['red', 'green', 'blue', 'black'];
+for(let i = 0; i < colors.length; i++){
+console.log(colors [i]);
+}*/
+
+/*const colors = ['red', 'green', 'blue'];
+for(let color of colors){
+console.log(color);
+}*/
+
+/*const message = 'Welcome to JavaScript';
+for(let character of message) {
+console.log (character) ;
+}*/
+
+/*const colors = ['red', 'green', 'blue'];
+let i = 0;
+while(i < colors.length) {
+console.log(colors [i]);
+i++;
+}*/
+
+/*const message = 'Welcome to JavaScript';
+let i = 5;
+do{
+console.log(message);
+i++;
+}while(i < 5);
+console.log('***' ) ;*/
+
+/*const numbers = [1, 2, 3, 0, 4, 5];
+for(let i = 0; i < numbers.length; i++) {
+console.log (numbers [i]);
+}
+console.log('*** ' );*/
+
+/*const numbers = [1, 2, 3, 0, 4, 5];
+for(let i = 0; i < numbers.length; i++) {
+if (numbers [i] == 0) {
+continue;
+}
+console.log(numbers [i]);
+}
+console.log('***' );*/
+
+/*function printNumbers() {
+    console.log(1);
+    console.log(2) ;
+    console.log (3);
+    }
+    printNumbers();*/
+
+
+    /*
+    function printNumbers() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+     }
+     printNumbers ();
+     printNumbers();
+     printNumbers();*/
+
+/*function printNumbers () {
+    for(let i = 1; i <= 4; i++) {
+    console.log (i);
+    }
+    }
+    printNumbers();
+     printNumbers ();
+     printNumbers();
+*/
+
+/*function printNumbers (to) {
+    for(let i = 1; i <= to; i++) {
+    console.log(i);
+    }
+    }
+    printNumbers (2);
+    printNumbers (4);
+    printNumbers (3) ;*/
+
+
+ /*function printNumbers (to) {
+        for(let i = 1; i <= to; i++){
+        console.log(i);
+        }
+        }
+        function add (firstNumber, secondNumber) {
+        console.log(firstNumber + secondNumber);
+        }
+        add (2, 7);
+        add (5, 3);*/
+
+    /* function printNumbers (to) {
+            for(let i = 1; i <= to; i++) {
+            console.log(i);
+            }
+            }
+            function add (firstNumber, secondNumber) {
+            console.log (firstNumber + secondNumber);
+            }
+            add ("Welcome to ", "JavaScript");
+            add (5, 3);
+
+*/
+
+/*function printNumbers (to) {
+    for(let i = 1; i <= to; i++){
+    console.log(i);
+    }
+    }
+    function add (firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+    }
+    let value = add (2, 7);
+    console.log(value);*/
+
+/*function printNumbers (to) {
+        for(let i = 1; i <= to; i++){
+        console.log(i);
+        }
+        }
+        function add (firstNumber, secondNumber) {
+        let result = firstNumber + secondNumber;
+        return result;
+        }
+        let value = add (2, 7) - add (2, 3);
+        console.log(value);*/
+
+/*function printNumbers (to) {
+            for(let i = 1; i <= to; i++){
+            console.log(i);
+            }
+            }
+            function add (firstNumber, secondNumber) {
+            return firstNumber + secondNumber;
+            }
+            printNumbers (add (2, 7));*/
+
+/*function printNumbers (to) {
+                for(let i = 1; i <= to; i++){
+                console.log(i);
+                }
+                return;
+                }
+                function add (firstNumber, secondNumber) {
+                return firstNumber + secondNumber;
+                }
+                printNumbers (5);*/
+
+/* function printNumbers (to) {
+                    if (to > 10) {
+   return;
+                    }
+                    for(let i = 1; i <= to; i++){
+                    console.log(i);
+                    }
+                    }
+                    function add (firstNumber, secondNumber) {
+                    return firstNumber + secondNumber;
+                    }
+                    printNumbers (5);
+                    printNumbers (11);
+                    */
+
+
+ var books = [
+ [1, "Start with why", "Simon Sinek", 80.0, 13],
+ [2, "But how do it know", "J. Clark Scott", 59.9, 22],
+ [3, "Clean Code", "Robert Cecil Martin", 50.0, 5],
+ [4, "Zero to One", "Peter Thiel", 45.0, 12],
+ [5, "You don't know JS", "Kyle Simpson", 39.9, 9]
+];
+                    
+function addBook(id, title, author, price, quantity) {
+books.push([id, title, author, price, quantity])
+}
+                    
+ function showBooks() {
+ for (var i = 0; i < books.length; i++) {
+ console.log(books[i])
+}
+}
+                    
+function findBookByTitle(title) {
+ for (var i = 0; i < books.length; i++) {
+ if (books[i][1] === title) {
+return
+ }
+  }
+ console.log("Book not found")
+ }
+                    
+  function sellBook(title, quantity, customerBalance) {
+  for (var i = 0; i < books.length; i++) {
+  if (books[i][1] === title) {
+   if (books[i][4] >= quantity) {
+     var totalCost = books[i][3] * quantity
+   if (customerBalance >= totalCost) {
+     books[i][4] -= quantity
+     console.log("Book sold. Remaining quantity: " + books[i][4])
+       return
+    } else {
+   console.log("Not enough balance.")
+   return
+     }
+  } else {
+   console.log("Not enough quantity.")
+ return
+}
+ }
+ }
+console.log("Book not found.")
+   }
+                    
+ addBook(6, "New Book", "New Author", 30.0, 10)
+showBooks()
+ findBookByTitle("Clean Code")
+ sellBook("Clean Code", 2, 100)
+                    
+                    
